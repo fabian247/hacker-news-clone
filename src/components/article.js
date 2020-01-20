b3d5323b96f9e281ccebd06ebb71a9d6a0f17631
Fabian <freelance@fabianfrank.it>
2020-01-20 15:23:40 +0100

    Add article component
    Fetches data for article

---
+++ src/components/article.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+import superagent from 'superagent'
+import propTypes from 'prop-types'
+
+const articleUrl = (id) => `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+
+const getArticle = async (id) => {
+  try {
+    const res = await superagent.get(articleUrl(id))
+    const article = res.body
+    return article
+  } catch (e) {
+    console.error(e)
+    throw e
+  }
+}
+
+const Article = ({ id }) => {
+  const [article, setArticle] = useState([])
+  useEffect(
+    () => {
+      async function fetchArticle() {
+        const article = await getArticle(id)
+        setArticle(article)
+      }
+
+      fetchArticle()
+    }, [id],
+  )
+
+  return (
+    <div>
+      {article.title}
+    </div>
+  )
+}
+
+Article.propTypes = {
+  id: propTypes.string.isRequired,
+}
+
+export { Article }
