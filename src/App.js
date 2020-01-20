4a299c3fabafdf66b68c8e862e9c2e33865218c5
Fabian <freelance@fabianfrank.it>
2020-01-20 15:10:52 +0100

    Use articles component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import { HeaderBar } from './components/header-bar'
+import { Articles } from './components/articles'
 
 function App() {
   return (
     <div className="app">
       <div className="app-content">
         <HeaderBar/>
+        <Articles/>
       </div>
     </div>
   );
