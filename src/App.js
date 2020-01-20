f2feb12e431ed1ed17a76af5d4f0ba6c2898eced
Fabian <freelance@fabianfrank.it>
2020-01-20 18:09:37 +0100

    Add routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React from 'react';
 import './App.css';
 import { HeaderBar } from './components/header-bar'
 import { Articles } from './components/articles'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { ArticleDetails } from './components/article-details'
+import { Switch } from 'react-router'
 
 function App() {
   return (
     <div className="app">
       <div className="app-content">
         <HeaderBar/>
-        <Articles/>
+        <Router>
+          <Switch>
+            <Route path="/item/:id">
+              <ArticleDetails/>
+            </Route>
+            <Route path="/">
+              <Articles/>
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </div>
   );
